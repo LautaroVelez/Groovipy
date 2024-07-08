cd140615ff4ab726d0db07f5cadc62f16d419291
LautaroVelez <lautivelez28@gmail.com>
2024-07-07 22:53:24 -0300

    created next

---
+++ groovi/pages/landing/index.js
@@ -0,0 +1,9 @@
+'use client'
+import {Button, ButtonGroup} from "@nextui-org/button";
+export default function Landing() {
+    return(
+        <>
+            <Button size="lg"></Button>
+        </>
+    )
+}
